27411040bbf3616e163560d9015b27364f4f1dd9
Sean Baines <mrseanbaines@gmail.com>
2021-06-30 22:06:55 +0100

    Initial commit

---
+++ projects/blog/.cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,9 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/mrseanbaines-pleo/projects/pleo/gatsby-monorepo-test/projects/blog/.cache/dev-404-page.js"))
+}
+
